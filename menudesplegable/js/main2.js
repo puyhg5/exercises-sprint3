7fff9f75970bb46f15f9bdd358d264d76c2fcb43
puyhg5 <puyab500@gmail.com>
2017-05-24 17:50:24 +0200

    two navs and resize

--- menudesplegable/js/main2.js
+++ menudesplegable/js/main2.js
@@ -1,7 +1,7 @@
 var buttonClick = document.querySelector(".burguer-menu");
+var items = document.querySelectorAll(".nav-item");
 
 function toggleMenu() {
-var items = document.querySelectorAll(".nav-item");
 var delay = 0;
 for (var i = 0; i < items.length; i++) {
   items[i].style.transitionDelay = delay+"s";
@@ -11,3 +11,13 @@ for (var i = 0; i < items.length; i++) {
 }
 
 buttonClick.addEventListener("click",toggleMenu);
+
+function closeNavResponsive(){
+  if(window.innerWidth > 780){
+    for (var i = 0; i < items.length; i++) {
+      items[i].classList.remove('active');
+    }
+  }
+}
+
+window.addEventListener("resize",closeNavResponsive);
